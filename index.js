940b9bbbcf0b3de94a3a96ccb64300ea40114900
hartonoosu <hartonoosu@gmail.com>
2025-08-06 22:16:02 +1000

    feat: link openWeather API and render the weather

--- index.js
+++ index.js
@@ -5,9 +5,55 @@ const port = 3000;
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs", {})
+app.get("/", async (req, res) => {
+    res.render("index.ejs", {
+        location: "dunno yet", 
+        temperature: "-", 
+        icon:"?",
+        weather: "weather",
+        city: "city"
+    })
+})
+
+app.post("/get-forecast", async (req,res) => {
+    const apiKey = "1ff2e987c420ff26cea44dde73aa4efa";
+
+    let lat = req.body.lat;
+    let lon = req.body.lon;
+    let city = req.body.city;
+
+    if (!lat || !lon){
+        lat = (Math.random()*180 - 90).toFixed(6);
+        lon = (Math.random()*360 - 90).toFixed(6);
+    }
+
+    try {
+        const weatherAPI = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={part}&units=metric&appid=${apiKey}`)
+        const locationAPI = await axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`)
+
+        const location = weatherAPI.data.timezone;
+        const temperature = Math.round(weatherAPI.data.current.temp);
+        const iconURL = `https://openweathermap.org/img/wn/${weatherAPI.data.current.weather[0].icon}@2x.png`;
+        const weather = weatherAPI.data.current.weather[0].description;
+        const country = locationAPI.data[0].state;
+        console.log(`city: ${country}`)
+
+        console.log(temperature);
+        console.log(weatherAPI.data.current.weather[0].icon);
+        console.log(iconURL)
+        res.render("index.ejs", {
+            location: location,
+            temperature: temperature,
+            icon: iconURL,
+            weather: weather,
+            city: country  
+        })
+    }
+    catch (error) {
+        console.log(error.message);
+    }
 })
 
 app.listen(port, () => {
