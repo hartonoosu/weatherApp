22a3e972779ef09da61b7c0dfa4f27159d863700
hartonoosu <hartonoosu@gmail.com>
2025-08-07 21:12:08 +1000

    feat: input based on city name & add map iframe

--- index.js
+++ index.js
@@ -20,35 +20,35 @@ app.get("/", async (req, res) => {
 app.post("/get-forecast", async (req,res) => {
     const apiKey = "1ff2e987c420ff26cea44dde73aa4efa";
 
-    let lat = req.body.lat;
-    let lon = req.body.lon;
-    let city = req.body.city;
-
-    if (!lat || !lon){
-        lat = (Math.random()*180 - 90).toFixed(6);
-        lon = (Math.random()*360 - 90).toFixed(6);
-    }
+    // if (!lat || !lon){
+    //     lat = (Math.random()*180 - 90).toFixed(6);
+    //     lon = (Math.random()*360 - 90).toFixed(6);
+    // }
 
     try {
+        const city = req.body.city;
+        const locationAPI = await axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`);
+        const country = locationAPI.data[0].state;
+        const lat = locationAPI.data[0].lat;
+        const lon = locationAPI.data[0].lon;
+
         const weatherAPI = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={part}&units=metric&appid=${apiKey}`)
-        const locationAPI = await axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`)
 
         const location = weatherAPI.data.timezone;
         const temperature = Math.round(weatherAPI.data.current.temp);
         const iconURL = `https://openweathermap.org/img/wn/${weatherAPI.data.current.weather[0].icon}@2x.png`;
         const weather = weatherAPI.data.current.weather[0].description;
-        const country = locationAPI.data[0].state;
-        console.log(`city: ${country}`)
 
-        console.log(temperature);
-        console.log(weatherAPI.data.current.weather[0].icon);
-        console.log(iconURL)
+        console.log(weatherAPI.data.lat, weatherAPI.data.lon)
+
         res.render("index.ejs", {
             location: location,
             temperature: temperature,
             icon: iconURL,
             weather: weather,
-            city: country  
+            city: country,
+            lat: lat,
+            lon: lon 
         })
     }
     catch (error) {
